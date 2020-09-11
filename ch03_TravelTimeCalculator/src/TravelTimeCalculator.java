535f1d4a3a683d462c9548d2083685242745a832
Dana-Kimmel <dear.prudence34@gmail.com>
2020-09-10 21:13:49 -0400

    init commit

---
+++ ch03_TravelTimeCalculator/src/TravelTimeCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TravelTimeCalculator {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Travel Time Calculator");
+		System.out.println();
+
+		Scanner sc = new Scanner(System.in);
+
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+
+			System.out.print("Enter miles: ");
+			double miles = sc.nextDouble();
+
+			System.out.print("Enter miles per hour: ");
+			double mph = sc.nextDouble();
+
+			int hours = (int) ((int) miles / mph);
+			int min = (int) (((miles / mph) * 60) % 60);
+
+			System.out.println("Estimated travel time: ");
+			System.out.println("---------------------");
+			System.out.println("Hours: " + hours);
+			System.out.println("Minutes " + min);
+
+			System.out.println("Continue (y/n): ");
+			choice = sc.next();
+
+			System.out.println("Goodbye:)");
+		}
+	}
+
+}
