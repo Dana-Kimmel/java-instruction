731fa4b369fc31bb3ca111056ecbd0f8f4f2ef0c
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-09 16:21:09 -0500

    init commit

---
+++ ch07_DiceRoller/src/kimmel/app/Dice.java
@@ -0,0 +1,5 @@
+package kimmel.app;
+
+public class Dice {
+
+}
---
+++ ch07_DiceRoller/src/kimmel/app/Die.java
@@ -0,0 +1,27 @@
+package kimmel.app;
+
+public class Die {
+
+	// fields
+	private int die;
+
+	// constructors
+	public Die() {
+		die = 0;
+	}
+
+	public Die(int die) {
+		this.die = die;
+	}
+
+	// getter
+	public int getValue() {
+		return die;
+	}
+
+	// helper method to roll die
+	public void roll() {
+		int die = (int) (Math.random() * 6) + 1;
+	}
+
+}
